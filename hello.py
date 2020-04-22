7bf5aa597289e5f48194bbacc83b9d378bb1ed04
Damian Pagowski <d.pagowski@gmail.com>
2020-04-22 21:22:10 +0700

    math operator

--- hello.py
+++ hello.py
@@ -6,3 +6,29 @@ import sys
 name = "Stranger" if len(sys.argv) < 2 else sys.argv[1] 
 print("hello {}".format(name))
 
+#  comments
+#  "pound character"
+
+# math operators
+# same as js, but division is a bit different // vs /
+# //drops fractional part after decimal
+print(7//2)
+print(7.0//2.0)
+print(7.0//2)
+print(7/2)
+print(7.0/2)
+print(7/4)
+print(7.0/4)
+
+
+# string templates
+
+
+# data types
+
+# conditionals
+
+# loops
+
+# data structures
+
